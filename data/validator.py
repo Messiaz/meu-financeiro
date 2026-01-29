e7a645708554bce585d09a5a1f7bfa5769cad15b
Messias <41300015+Messiaz@users.noreply.github.com>
2026-01-29 12:51:39 -0300

    Update validator.py

--- data/validator.py
+++ data/validator.py
@@ -1,29 +1,18 @@
 import pandas as pd
-import os
 
 CAT_ENTRADAS = ["Salário", "Aulas Particulares - MEI", "Rendimento", "Transferência", "Outros"]
 CAT_SAIDAS = ["Alimentação", "Lazer", "Investimento", "Saúde (Dedutível)", "Educação (Dedutível)", 
-              "Livros", "Remédios", "Estudos", "Trabalho", "Transporte", "Manutenção", "MEI", "Impostos", "Outros"]
+              "Livros", "Remédios","Estudos", "Trabalho", "Transporte", "Manutenção", "MEI", "Impostos", "Outros"]
 
 def predict_data(df, df_hist=None):
     if df.empty: return df
+    ids_banco = df_hist['ID_Transacao'].astype(str).unique() if df_hist is not None and not df_hist.empty else []
     
-    ids_banco = []
-    if df_hist is not None and not df_hist.empty:
-        ids_banco = df_hist['ID_Transacao'].astype(str).unique()
-    
-    df['Status'] = df['ID_Transacao'].astype(str).apply(
-        lambda x: "❌ JÁ SALVO" if x in ids_banco else "✅ NOVO"
-    )
-    
-    df['Descrição_Visual'] = df.apply(
-        lambda x: f"⚠️ {x['Descrição']}" if x['Status'] == "❌ JÁ SALVO" else x['Descrição'], axis=1
-    )
-
+    df['Status'] = df['ID_Transacao'].astype(str).apply(lambda x: "❌ JÁ SALVO" if x in ids_banco else "✅ NOVO")
+    df['Descrição_Visual'] = df.apply(lambda x: f"⚠️ {x['Descrição']}" if x['Status'] == "❌ JÁ SALVO" else x['Descrição'], axis=1)
     df['Contabilizar'] = False 
     df['Segmento'] = "PF"
     # df['Tipo'] = df['Valor'].apply(lambda x: '🟢 Crédito' if x > 0 else '🔴 Débito')
     df['Tipo'] = df['Valor'].apply(lambda x: 'Débito')
     df['Categoria'] = df['Valor'].apply(lambda x: "Salário" if x > 0 else "Outros")
-    
     return df
